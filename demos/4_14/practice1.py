016b099d254b1400c7f24b6d292062c19d5ad304
siyuliang <47640090+siyuliang@users.noreply.github.com>
2026-04-12 19:46:35 -0700

    updated demos

---
+++ demos/4_14/practice1.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    # Write an expression for the following prose:
+    # “Three plus two minus ten all divided by fifteen”
+    
+    # On a new line, store the value of that expression in a variable
+    # called “x”
+    
+    # Assign the string 'hello' to a variable called “var”
+    
+    # Print the result of the following expression:
+    # “(3**2+4**2)**0.5” (without the quotation marks)
---
+++ demos/4_14/practice2.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    # Create a list that includes the integers from 1-9
+
+    # Print the fourth element from that list
+
+    # Print the fourth and fifth element from that list using slicing
+
+    # Set the last element to 10
+    
+    # Create a string of the integers from 1-9
+
+    # Print the fourth character from that string
+
+    # Print the fourth and fifth characters from that string using slicing
+
+    # Print a string where the '9' is replaced with '10'
